4bca2c5ad8c2be93e32ed547104d3a60e1c606b1
jonmeidell <jonmeidell@gmail.com>
2020-05-15 21:52:46 -0600

    added product list

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,20 @@ const connection = mysql.createConnection({
     database: "bamazon"
 });
 
+connection.connect();
+
+connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+    if (err) console.log("Error in command.");
+    let productList = new table({
+        head: ["Item ID", "Product Name", "Price $"]
+    });
+    for(let i = 0; i < res.length; i++) {
+        productList.push(
+            [res[i].item_id, res[i].product_name, res[i].price]
+        );
+    };
+    console.log(productList.toString()+ "\n");
+    purchaseItem();
+});
+
+function purchaseItem();
