d6edc46462986085df4334ad48aed8e14ebc339c
jonmeidell <jonmeidell@gmail.com>
2020-05-19 18:37:14 -0600

    fixes to ordering function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,21 +15,22 @@ connection.connect();
 listItems();
 
 function listItems() {
-    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+    connection.query("SELECT item_id, product_name, stock_quantity, price FROM products", function (err, res) {
         if (err) console.log("Error in command.");
 
         for (let i = 0; i < res.length; i++) {
             productList.push(
-                [res[i].item_id, res[i].product_name, res[i].price]
+                res[i]
             );
+            console.log("ID: " + res[i].item_id + ", qty: " + res[i].stock_quantity + ", name: " + res[i].product_name + ", price: $" + res[i].price);
         };
-        console.log(productList.toString() + "\n");
+        // console.log(productList.toString() + "\n");
         purchaseItem()
     });
     connection.end();
 };
 
-function purchaseItem(); {
+function purchaseItem() {
     inquirer.prompt([
         {
             type: "input",
@@ -43,16 +44,16 @@ function purchaseItem(); {
             name: "quantity",
             filter: Number
         },
-        {
-            type: "confirm",
-            message: "Is this order correct?" + itemID.name + "Quantity of " + itemID.quantity,
-            name: "order",
-            default: false
-        }
+        // {
+        //     type: "confirm",
+        //     message: "Is this order correct? " + productList[itemID].product_name + " quantity of " + productList[itemID].stock_quantity,
+        //     name: "order",
+        //     default: false
+        // }
     ])
         .then(function (orderCheck) {
-            if (orderCheck.order) {
-
-            }
+            // if (orderCheck.order) {
+            console.log(orderCheck);
+            console.log("Your order total is: $" + (orderCheck.quantity * parseInt(productList[orderCheck.itemID-1].price)));
         })
 };
